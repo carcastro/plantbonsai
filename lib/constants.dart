bccdd2de2226b5e576b9e76f664f841006ac29cf
carlos castro <carcastro5522@gmail.com>
2020-09-16 23:57:20 -0300

    cambios en el color de la app

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // Colores que usamos en nuestra aplicación
-const kPrimaryColor = Color(0xFF80CBC4);
+const kPrimaryColor = Color(0xFF8BC34A);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
